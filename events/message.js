90833484eced6ba29d21136d1d4daf17115c3bee
Carlos Cerna <cbcerna@ksu.edu>
2020-09-16 14:38:18 -0500

    Added new functions, removed old usings and test statements

--- events/message.js
+++ events/message.js
@@ -3,4 +3,16 @@ module.exports = (client, message) => {
   if (message.content.startsWith("!kick")) {
     return kick(message)
   }
+
+  if (message.content === "ping") {
+    message.reply("Pong!")
+  }
+
+  if (message.content === "Hello") {
+    message.reply("Hiya!")
+  }
+
+  if (message.content === "What are you?") {
+    message.reply("My name is Carmen and I am Bot! :yum: ")
+  }
 }
--- index.js
+++ index.js
@@ -3,24 +3,12 @@ const Discord = require("discord.js")
 const fs = require("fs")
 const client = new Discord.Client()
 
-// fs.readdir("events/", (err, files) => {
-//   files.forEach((file) => {
-//     const eventHandler = require(`events/${file}`)
-//     const eventName = file.split(".")[0]
-//     client.on(eventName, (...args) => eventHandler(client, ...args))
-//   })
-// })
-
-client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
-  }
-})
-
-client.on("message", (msg) => {
-    if (msg.content === "Hello") {
-      msg.reply("Hiya!")
-    }
+fs.readdir("events/", (err, files) => {
+  files.forEach((file) => {
+    const eventHandler = require(`./events/${file}`)
+    const eventName = file.split(".")[0]
+    client.on(eventName, (...args) => eventHandler(client, ...args))
+  })
 })
 
 client.login(process.env.BOT_TOKEN)
