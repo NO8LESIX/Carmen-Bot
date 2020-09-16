32e8ee93c452740df2426ec09e59d9b51a3e40b5
Carlos Cerna <cbcerna@ksu.edu>
2020-09-16 14:23:45 -0500

    Updates to Carmen's File system

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config()
+const Discord = require("discord.js")
+const fs = require("fs")
+const client = new Discord.Client()
+
+// fs.readdir("events/", (err, files) => {
+//   files.forEach((file) => {
+//     const eventHandler = require(`events/${file}`)
+//     const eventName = file.split(".")[0]
+//     client.on(eventName, (...args) => eventHandler(client, ...args))
+//   })
+// })
+
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!")
+  }
+})
+
+client.on("message", (msg) => {
+    if (msg.content === "Hello") {
+      msg.reply("Hiya!")
+    }
+})
+
+client.login(process.env.BOT_TOKEN)
