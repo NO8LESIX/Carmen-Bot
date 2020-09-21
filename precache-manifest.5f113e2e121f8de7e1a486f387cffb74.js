3f91930ce321176a004cced5cc25a44a4001e343
Carlos Cerna <cbcerna@ksu.edu>
2020-09-21 15:11:12 -0500

    Updates

---
+++ precache-manifest.5f113e2e121f8de7e1a486f387cffb74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50c927e983b06be8cf3c00c1cb6a37c",
+    "url": "/Carmen-Bot/index.html"
+  },
+  {
+    "revision": "82543e46307f5c2ad1d3",
+    "url": "/Carmen-Bot/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "023a656d2649d18a5b69",
+    "url": "/Carmen-Bot/static/js/2.f38f5518.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Carmen-Bot/static/js/2.f38f5518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82543e46307f5c2ad1d3",
+    "url": "/Carmen-Bot/static/js/main.b315cce2.chunk.js"
+  },
+  {
+    "revision": "5169e62deb114a95a7d6",
+    "url": "/Carmen-Bot/static/js/runtime-main.fe6840d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Carmen-Bot/static/media/logo.5d5d9eef.svg"
+  }
+]);
